1fd7661b5039e71156583f2784c8198ef64843fe
caiopereira-source <caio.pereira2302@gmail.com>
2024-10-09 11:52:57 -0300

    feat: started customers CRUD

---
+++ src/main/java/br/com/caiopereira_source/rentabook/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package br.com.caiopereira_source.rentabook.controller;
+
+
+import br.com.caiopereira_source.rentabook.dto.CustomerDto;
+import br.com.caiopereira_source.rentabook.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService service;
+
+    @PostMapping
+    public ResponseEntity<CustomerDto> create(@RequestBody CustomerDto customerDto){
+        CustomerDto customer = service.create(customerDto);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDto> findById(@PathVariable(name = "id") long id){
+        CustomerDto customer = service.findById(id);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> findAll(){
+        var list = service.findAll();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
--- src/main/java/br/com/caiopereira_source/rentabook/service/CustomerService.java
+++ src/main/java/br/com/caiopereira_source/rentabook/service/CustomerService.java
@@ -1,11 +1,16 @@
 package br.com.caiopereira_source.rentabook.service;
 
+import br.com.caiopereira_source.rentabook.dto.CityDto;
 import br.com.caiopereira_source.rentabook.dto.CustomerDto;
+import br.com.caiopereira_source.rentabook.exception.ResourceNotFoundException;
 import br.com.caiopereira_source.rentabook.mapper.CustomModelMapper;
+import br.com.caiopereira_source.rentabook.model.CityModel;
 import br.com.caiopereira_source.rentabook.model.CustomerModel;
 import br.com.caiopereira_source.rentabook.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
@@ -18,4 +23,32 @@ public class CustomerService {
         return CustomModelMapper.parseObject(repository.save(customer), CustomerDto.class);
     }
 
+    public CustomerDto findById(long id){
+        return CustomModelMapper.parseObject(repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado!")), CustomerDto.class);
+    }
+
+    public CustomerDto update(CustomerDto customerDto){
+        CustomerModel found = repository.findById(customerDto.getId()).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado!"));
+        found.setFullName(customerDto.getFullName());
+        found.setGender(customerDto.getGender());
+        found.setBirthDate(customerDto.getBirthDate());
+
+        // *_*
+        CityModel city = repository.findById(customerDto.getCity().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Cidade não encontrada!")).getCity();
+
+        found.setCity(city);
+
+        return CustomModelMapper.parseObject(repository.save(found), CustomerDto.class);
+    }
+
+    public void delete(long id){
+        CustomerModel found = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado!"));
+        repository.delete(found);
+
+    }
+
+    public List<CustomerDto> findAll(){
+        return CustomModelMapper.parseObjectList(repository.findAll(), CustomerDto.class);
+    }
 }
